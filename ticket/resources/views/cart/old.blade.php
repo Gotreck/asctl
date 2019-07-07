b7964d7f0a30d00d4d8da9a336d0e687368e9eb3
sebastien68 <obigame68@gmail.com>
2019-07-07 16:08:41 +0200

    patch old cart

--- ticket/resources/views/cart/old.blade.php
+++ ticket/resources/views/cart/old.blade.php
@@ -11,19 +11,16 @@
 
 <div class="row" id="ticket-list">
     <div class="col l6 offset-l3 m10 offset-m1 s10 offset-s1">
-        <div class="col l12 m12">
-            <h2>{{__("Rechnungen")}}</h2>
-        </div>
-        <div class="col l2 m3">
+        <div class="col l2 m3 hide-on-small-only">
             <h3>{{__("Bestellnummer")}}</h3>
         </div>
-        <div class="col l4 m3">
+        <div class="col l4 m3 hide-on-small-only">
             <h3>{{__("Bestelldatum")}}</h3>  
         </div>
-        <div class="col l3 m3">
+        <div class="col l3 m3 s4 offset-s4">
             <h3>{{__("Bestellwert")}}</h3>  
         </div>
-        <div class="col l3 m3">
+        <div class="col l3 m3 s4">
             <h3>{{__("Dokumente")}}</h3>
         </div>
     </div>
@@ -35,16 +32,16 @@
     @foreach ($carts as $cart)
 
     <div class="col l6 offset-l3 m10 offset-m1 s10 offset-s1">
-        <div class="col l2 m3">
+        <div class="col l2 m3 s4">
             <h3>{{$cart->id}}</h3>
         </div>
-        <div class="col l4 m3">
+        <div class="col l4 m3 hide-on-small-only">
             <h3>    {{$cart->updated_at}}</h3>  
         </div>
-        <div class="col l3 m3">
+        <div class="col l3 m3 s4">
             <h3>{{$cart->totalprice()}} CHF</h3>  
         </div>
-        <div class="col l3 m3">
+        <div class="col l3 m3 s4">
             <h3><a href="/order/pdf/{{$cart->id}}">  <i class="material-icons">file_download</i></a></h3>
         </div>
     </div>
